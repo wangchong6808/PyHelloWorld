7aeeced72da182ae02a9540a4f3573c563f009ef
WangChong <wangchong6808@163.com>
2018-09-03 09:44:16 +0800

    exception

--- src/exception.py
+++ src/exception.py
@@ -1,10 +1,34 @@
+class MyError(Exception):
+
+    def __str__(self):
+        return 'this is my error'
+
+
 def divide(n, m):
     try:
         x = n / m
         print('result is', x)
-    except ZeroDivisionError:
-        print('something wrong happend')
+    except (ZeroDivisionError, NameError) as e:
+        print('something wrong happend', e)
+    except SyntaxError:
+        print('syntax error')
+    else:
+        print('no error happened')
+    finally:
+        print('program is finished')
 
 
+def raise_my_error(m):
+    try:
+        print('m is', m)
+        raise MyError()
+    except MyError as e:
+        print('the error is ', e)
+
+
+# try else finally
 divide(100, 10)
+# try except finally
 divide(100, 0)
+
+raise_my_error('')
